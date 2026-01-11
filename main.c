0e49d22605c895110b5ee8b6a5c7934965ebf28b
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 11:21:08 +0300

    Implement digit counting and upper bound generation logic
    Added source code that calculates the digit count ($d$) of an input and dynamically constructs the maximum integer value ($10^d - 1$) using iterative output generation.

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi,sayac=0;
+
+    printf("sayi girin ");
+    scanf("%d",&sayi);
+
+    if(sayi<0){
+        sayi=-sayi;
+    }
+    if(sayi==0){
+        sayac=1;
+    }
+    else{
+    for(;sayi>0;sayi/=10){
+        sayac++;
+    }
+    }
+    printf("basamak %d\n",sayac);
+
+    printf("sonuc: ");
+
+    for(;sayac>0;sayac--){
+        printf("9");
+    }
+}
